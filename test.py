95d5cc36be4eb4f3d39cf24c997993c8319aa24d
[Gil Nims] <[gilnims@gmail.com]>
2018-09-12 13:09:27 -0400

    Syntax changes and comments
    Made syntax changes after python interpreter complained about indentation. Also added some comments at the bottom.

--- test.py
+++ test.py
@@ -20,34 +20,34 @@ Use 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736 for use
 key.extend(list(string.ascii_lowercase)+list(string.ascii_uppercase))
 
 for letter in key:
-    for byte in encoded_string:
-        decode = chr(ord(letter) ^ byte)
-        for i in decode:
-            if i in decoded.keys():
-                decoded[i] += 1
-            else:
-                decoded[i] = 1
+	for byte in encoded_string:
+		decode = chr(ord(letter) ^ byte)
+		for i in decode:
+			if i in decoded.keys():
+				decoded[i] += 1
+			else:
+				decoded[i] = 1
     #print(decoded.keys())
-    for i in decoded.keys():
-        value = ord(i)
-        if 65 <= value <= 90 or 97 <= value <= 122:
-            chars += 1
-        else:
-            non_chars += 1
-    score = (non_chars/chars)*100
-    if score <= 15:
-        print(letter + ':' + "*" * 50) # Added this line to denote which letter did the decrypt
-        print('\n')
-        print(decoded)
-        print('\n')
-        print("The non-character score is: " + str(int(score)) + " The lower the better.")
-        decoded = {}
-        chars = 0
-        non_chars = 0
-    else:
-        decoded = {}
-        chars = 0
-        non_chars = 0
+	for i in decoded.keys():
+		value = ord(i)
+		if 65 <= value <= 90 or 97 <= value <= 122:
+			chars += 1
+		else:
+			non_chars += 1
+	score = (non_chars/chars)*100
+	if score <= 15:
+		print(letter + ':' + "*" * 50) # Added this line to denote which letter did the decrypt
+		print('\n')
+		print(decoded)
+		print('\n')
+		print("The non-character score is: " + str(int(score)) + " The lower the better.")
+		decoded = {}
+		chars = 0
+		non_chars = 0
+	else:
+		decoded = {}
+		chars = 0
+		non_chars = 0
 
 decode_letter = input('What letter do you want to use to decode this stream? ')
 for byte in encoded_string:
@@ -55,12 +55,12 @@ for byte in encoded_string:
 
         
         
-"""        
-        list.insert(list_pos, ord(letter) ^ byte)
-        list_pos += 1
-        #print(letter + " * " * len(encoded_string))
-    #for item in list:
-    print(chr(list), end = " ")
-    #print(letter + "-" + "*" * 50, end='')
-        #print(decoded, sep='', end='')
-"""
+'''
+Use this code instead to determine XOR value:
+
+	Also, do different functions for XOR, score, and key_xor (letter to decrypt)
+
+for i in range(256):
+        b2 = [i] * len(b1)
+        plaintext = bytes(xor(b1, b2))
+        pscore = score(plaintext)
