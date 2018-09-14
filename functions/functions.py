664233a0cd0ca92f406d3a37014cbb4049c55df9
[Gil Nims] <[gilnims@gmail.com]>
2018-09-14 13:17:15 -0400

    Added more functions

--- functions/functions.py
+++ functions/functions.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env Python3
 
+#  #1
 def max_of_two(x, y):
 	if x > y:
 		return x
 	return y
 #print(max_of_two(0, -1))
 
+#  #2
 def list_sum(sum):
 	sum = 0
 	for num in num_list:
@@ -15,6 +17,7 @@ def list_sum(sum):
 #print(list_sum(sum)) My code
 #print(sum((8, 2, 3, 0, 7))) # print(sum()) returns  an error - sum expected at most 2 arguments, got 5
 
+#  #3
 def multiply(numbers):
 	product = 1
 	for num in numbers:
@@ -22,7 +25,7 @@ def multiply(numbers):
 	return numbers
 #print(multiply((8, 2, 3, -1, 7)))
 
-
+#  #4
 def string_reverse(str1):
 	rstr1 = ''
 	index = len(str1)
@@ -37,3 +40,12 @@ def string(reverse):
 	rstr1 = string_reverse[::-1]
 	return rstr1
 print(string_reverse('1234abcd')'''
+
+#  #5
+import math
+num = int(input('Please input a number: '))
+x = 1
+def factorial(x):
+	x = math.factorial(num)
+	return x
+print('The factorial of {} is: {}'.format(num, x))
